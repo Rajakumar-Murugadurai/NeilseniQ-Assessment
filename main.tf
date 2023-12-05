6126d5bd0ec9e9c2cd6cc453a0e6f7d4213c3761
Rajakumar Murugadurai <rakkumaranrjkumar@gmail.com>
2023-12-05 18:00:00 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,55 @@
 # Configure the Azure provider
+#terraform {
+#  required_providers {
+#    azurerm = {
+#      source  = "hashicorp/azurerm"
+#      version = "~> 3.0.2"
+#    }
+#  }
+
+#  required_version = ">= 1.1.0"
+#  backend "azurerm" {}
+
+#}
+
+#provider "azurerm" {
+#  features {}
+#}
+
+
+
+#resource "azurerm_resource_group" "rg" {
+#  name     = "niq-rg"
+#  location = var.location-rg
+#  tags = {
+#    "Application" = "DemoAppNiq"
+#  }
+#}
+
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = ">=2.0.0"
+      skip_provider_registration = true
     }
   }
 
-  required_version = ">= 1.1.0"
-  backend "azurerm" {}
-
-}
-
-provider "azurerm" {
   features {}
 }
 
+provider "azurerm" {
+  features = {}
+}
 
+# Your existing provider configuration
+provider "azurerm" {
+  # Other provider configurations...
+
+  # Adjust the following based on your actual subscription ID and tenant ID
+  subscription_id = "381e41a3-851a-4f47-9c43-4084c9be9bdc"
+  tenant_id       = "28f38b70-580e-4091-921f-4b06b4d6f5fc"
+}
 
 resource "azurerm_resource_group" "rg" {
   name     = "niq-rg"
@@ -24,6 +57,8 @@ resource "azurerm_resource_group" "rg" {
   tags = {
     "Application" = "DemoAppNiq"
   }
-}
+
+
+
 
 
