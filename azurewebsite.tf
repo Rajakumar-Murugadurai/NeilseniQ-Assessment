c9dd889e2f492936a965e7c7c8d67294c2183ab4
Rajakumar Murugadurai <rakkumaranrjkumar@gmail.com>
2023-12-05 15:17:49 +0000

    Updated azurewebsite.tf

--- azurewebsite.tf
+++ azurewebsite.tf
@@ -1,7 +1,7 @@
 #Frontend
 # Create the web app, pass in the App Service Plan ID
 resource "azurerm_linux_web_app" "fe-webapp" {
-  name                  = "fitnessgeek"
+  name                  = "svc"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   service_plan_id       = azurerm_service_plan.fe-asp.id
@@ -31,7 +31,7 @@ resource "azurerm_linux_web_app" "fe-webapp" {
 #Backend
 #storage account for functionapp
 resource "azurerm_storage_account" "fn-storageaccount" {
-  name                     = "fgfunctionappsa2023"
+  name                     = "fgfunctionappsaniq2023"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
