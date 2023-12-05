26499c00b1039e3a4e27e9beba967774413ad977
Rajakumar Murugadurai <rakkumaranrjkumar@gmail.com>
2023-12-05 15:23:07 +0000

    Updated vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,6 @@
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "fitnessgeek-vnet-test"
+  name                = "niq-vnet-test"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
